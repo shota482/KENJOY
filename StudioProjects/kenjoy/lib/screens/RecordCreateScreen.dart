60242abdee5a66c6c5d1410b4b0e2759775d9579
shota482 <syouta0519yamaguchi@gmail.com>
2025-10-28 22:49:13 +0900

    add: Record create page

---
+++ StudioProjects/kenjoy/lib/screens/RecordCreateScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RecordCreatePage extends StatefulWidget{
+  const RecordCreatePage({super.key});
+
+  @override
+  State<RecordCreatePage> createState() => _RecordCreatePage();
+}
+
+class _RecordCreatePage extends State<RecordCreatePage>{
+  final
+}
