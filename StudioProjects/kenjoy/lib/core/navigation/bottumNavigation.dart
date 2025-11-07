d33cfee37c8b91a18843415db87f4a955264560e
shota482 <syouta0519yamaguchi@gmail.com>
2025-11-07 10:42:52 +0900

    fix:

--- StudioProjects/kenjoy/lib/core/navigation/bottumNavigation.dart
+++ StudioProjects/kenjoy/lib/core/navigation/bottumNavigation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kenjoy/features/record/create/RecordCreateScreen.dart';
+import 'package:kenjoy/features/record/map/mapReview.dart';
 
 
 import '../../screens/homeScreeen.dart';
@@ -13,7 +14,7 @@ class AppNavigationBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final location = GoRouterState.of(context).uri.toString();
     int selectedIndex = switch (true) {
-      _ when location.startsWith(Page1.path) => 1,
+      _ when location.startsWith(MapReviewPage.path) => 1,
       _ when location.startsWith(RecordCreatePage.path) => 2,
       _ => 0
     };
@@ -27,7 +28,7 @@ class AppNavigationBar extends StatelessWidget {
             context.go(HomePage.path);
             break;
           case 1:
-            context.go(Page1.path);
+            context.go(MapReviewPage.path);
             break;
           case 2:
             context.go(RecordCreatePage.path);
@@ -36,8 +37,10 @@ class AppNavigationBar extends StatelessWidget {
       },
       destinations: const[
         NavigationDestination(icon: Icon(Icons.home), label: 'ホーム'),
-        NavigationDestination(icon: Icon(Icons.map), label: '地図'),
-        NavigationDestination(icon: Icon(Icons.add), label: '作成'),
+        NavigationDestination(icon: Icon(Icons.map), label: 'map'),
+        NavigationDestination(icon: Icon(Icons.add), label: 'create'),
+        NavigationDestination(icon: Icon(Icons.format_list_bulleted), label: 'trip'),
+        NavigationDestination(icon: Icon(Icons.account_circle), label: 'Mypage'),
       ],
     );
   }
--- StudioProjects/kenjoy/lib/routers/router.dart
+++ StudioProjects/kenjoy/lib/routers/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:kenjoy/features/record/map/mapReview.dart';
 
 import '../core/navigation/bottumNavigation.dart';
 import '../features/record/create/RecordCreateScreen.dart';
@@ -44,6 +45,13 @@ final goRouter = GoRouter(
               MaterialPage(
             child: RecordCreatePage(),
           ),
+        ),
+        GoRoute(
+          path: MapReviewPage.path,
+          pageBuilder: (BuildContext context, GoRouterState? state) =>
+              MaterialPage(
+                child: MapReviewPage(),
+              ),
         ),
       ],
     ),
